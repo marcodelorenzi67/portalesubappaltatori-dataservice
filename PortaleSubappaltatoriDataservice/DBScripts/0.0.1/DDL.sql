1f2a6ef72af9f8edaed1acf37583ad27734c81a2
Tommaso Garimoldi <tommaso.garimoldi@webratio.com>
2026-06-10 18:51:54 +0200

    import omnia e gsetione categorie

--- PortaleSubappaltatoriDataservice/DBScripts/0.0.1/DDL.sql
+++ PortaleSubappaltatoriDataservice/DBScripts/0.0.1/DDL.sql
@@ -14,3 +14,10 @@ alter table "public"."sub_allegati"  add column  "data_alert"  date;
 -- Documento_DocumentoInScadenza [as32o]
 alter table "public"."sub_allegati"  add column  "allegati_oid"  int4;
 alter table "public"."sub_allegati"   add constraint fk_allegati_allegati foreign key ("allegati_oid") references "public"."sub_allegati" ("oid");
+
+-- Fornitore [cls2]
+alter table "public"."sub_fornitore"  add column  "importato"  bool;
+
+-- Fornitore_Azienda [as33o]
+alter table "public"."sub_fornitore"  add column  "azienda_oid"  int4;
+alter table "public"."sub_fornitore"   add constraint fk_fornitore_azienda foreign key ("azienda_oid") references "public"."azienda" ("oid");
