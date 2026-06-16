dfbd089bcca9edbd5838b2d11bf9e3c365967d7d
maurizioferrario-wr <maurizio.ferrario@webratio.com>
2026-06-16 12:59:03 +0200

    Modifiche varie su gestione nome file

---
+++ PortaleSubappaltatoriDataservice/WebContent/WEB-INF/groovy/rinominaNomeFile.groovy
@@ -0,0 +1,25 @@
+#input Blob fileIn, String nome, String cognome, String azienda, String pattern
+import com.webratio.data.blob.BlobFactory
+
+def estensione = ""
+
+def nomeFile = fileIn.getName();
+println "nomeFile " + nomeFile
+println "nome " + nome
+println "cognome " + cognome
+println "azienda " + azienda
+
+int indicePuntoFinale = nomeFile.lastIndexOf(".")
+
+if (indicePuntoFinale >= 0) {
+    estensione = nomeFile.substring(indicePuntoFinale)
+}
+
+def risultato = pattern
+        .replace("##nome##", nome ?: "")
+        .replace("##cognome##", cognome ?: "")
+        .replace("##azienda##", azienda ?: "")
+        
+def nomeFileFinale = risultato + estensione
+
+return nomeFileFinale
